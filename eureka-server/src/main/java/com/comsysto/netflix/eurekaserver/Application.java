d4d9ec46ab6476cfe33f8486c6235a94b416cd9e
kroemerc <christian.kroemer@comsysto.com>
2015-11-25 10:35:37 +0100

    add spring cloud dashboard for monitoring and the likes

--- eureka-server/src/main/java/com/comsysto/netflix/eurekaserver/Application.java
+++ eureka-server/src/main/java/com/comsysto/netflix/eurekaserver/Application.java
@@ -1,11 +1,15 @@
 package com.comsysto.netflix.eurekaserver;
 
+import com.github.vanroy.cloud.dashboard.config.EnableCloudDashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
+@EnableCloudDashboard
 public class Application {
 
     public static void main(String[] args) {
