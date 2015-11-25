333e94a7bfe036539887d590085e6a64b455ecdb
oliversommer <oliver.sommer@comsysto.com>
2015-11-25 11:26:32 +0100

    total sales data source

--- total-sales-data-source/src/main/java/com/comsysto/netflix/data/source/totalsales/Application.java
+++ total-sales-data-source/src/main/java/com/comsysto/netflix/data/source/totalsales/Application.java
@@ -8,10 +8,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
@@ -19,6 +22,9 @@ import java.util.Random;
 
 @EnableEurekaClient
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
+@Profile("dev")
 @EnableScheduling
 public class Application {
 
@@ -28,6 +34,7 @@ public class Application {
 
     @Autowired
     private RestTemplate restTemplate;
+
     @Value("${data.source.location}}")
     private String locationId;
     @Value("${data.source.success.rate}")
